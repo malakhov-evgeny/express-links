a076bdc590ead0bb1097ef7a56ea4d870d22a9a0
malakhov.e <malakhov.e@selectel.ru>
2026-07-18 19:08:48 +0300

    fix(expresslink): path

--- express-links/vite.config.ts
+++ express-links/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/express-links/",
+  base: "/express-links/express-links/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
