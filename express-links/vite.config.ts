d67f97faf36056a77a9a758ef9f84d0dfad6056a
malakhov.e <malakhov.e@selectel.ru>
2026-07-18 16:38:53 +0300

    feat(expresslink): add actions

--- express-links/vite.config.ts
+++ express-links/vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/express-links/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
